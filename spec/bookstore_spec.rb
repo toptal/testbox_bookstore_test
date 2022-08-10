7cc4f3463d8bf6e8e0571b9be2ea21cf986900d4
Juliusz Gonera <juliusz.gonera@toptal.com>
2022-08-10 08:55:01 +0200

    break specs

--- spec/bookstore_spec.rb
+++ spec/bookstore_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Bookstore API' do
   before(:each) { TestboxHelper.new.db_reset }
 
   it 'has two books at the start' do
-    expect(AppClient.new.books.count).to eq 2
+    expect(AppClient.new.books.count).to eq 1
   end
 
   it 'adds a book' do
@@ -23,7 +23,7 @@ RSpec.describe 'Bookstore API' do
 
   describe 'Book title' do
     it 'Should be upper case', skip: !feature_flags['bookstore-api']['upcase_titles'] do
-      expect(AppClient.new.books.first['title']).to eq('MORFIN')
+      expect(AppClient.new.books.first['title']).to eq('MORFINA')
     end
 
     it 'Should be regular case', skip: feature_flags['bookstore-api']['upcase_titles'] do
